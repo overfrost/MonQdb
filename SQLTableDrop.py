919aebc3584b98aa5fc825e525f1f9c7c5e853fb
Andrew <frost.andrewj@gmail.com>
2023-10-11 21:51:40 -0700

    DB and file upload

---
+++ SQLTableDrop.py
@@ -0,0 +1,18 @@
+#! Python3
+# File takes an html of a saved page from Bluegolf
+# Uses bs4 to parse the html and create two lists, one for players (ranked by finish)
+# and a list of scores, also ranked by finish
+
+# Import Beautifiul Soup
+import bs4
+import dbFunctions
+import numpy as np
+import pandas as pd
+
+connection = dbFunctions.create_db_connection('MonQ.db')
+
+c = connection.cursor()
+
+c.execute('DROP TABLE IF EXISTS [SandersonMonQ+Stats]')
+
+dbFunctions.dbclose(connection)
